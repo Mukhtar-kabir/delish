7ebcbb45012351e7bbef4adec8e13b7486d305b0
mukhtar <mukhtarkabirmuhammad32@gmail.com>
2023-06-10 18:48:46 +0100

    Adding review section

--- script.js
+++ script.js
@@ -29,3 +29,27 @@ var swiper = new Swiper('.home-slider', {
   // },
   loop: true,
 });
+
+var swiper = new Swiper('.review-slider', {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
